7af6206f7a16394c23360ab52c20a30eb43e9af9
VamsiRasani <Vamsi0009rasani@gmail.com>
2024-01-22 17:43:23 +0530

    innital chnages

--- Natural_API/Resources/DistributorToExecutiveResource.cs
+++ Natural_API/Resources/DistributorToExecutiveResource.cs
@@ -8,8 +8,7 @@ namespace Natural_API.Resources
         public string Id { get; set; }
         public string ExecutiveId { get; set; }
         public string DistributorId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+       
 
     }
 }

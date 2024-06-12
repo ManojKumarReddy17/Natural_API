c2ec4dc11e8de7c94393fe5c315c28fc78d20e25
reddykumari <reddykumarid68@gmail.com>
2024-06-12 18:40:12 +0530

    commit

--- Natural_Core/Models/DsrRetailor.cs
+++ Natural_Core/Models/DsrRetailor.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+#nullable disable
 namespace Natural_Core.Models
 {
 	public class DsrRetailor
 	{
         public string Id { get; set; }
         public string Retailor { get; set; }
+        public string Area { get; set; }
     }
 }
 
